ea17b0543798fdfec57acf8b7f659d03fa43325f
Chebufek <pasha128320@gmail.com>
2024-06-07 14:21:09 +0300

    Created empty project

---
+++ src/main/java/com/testingTask/justAi/JustAiApplication.java
@@ -0,0 +1,13 @@
+package com.testingTask.justAi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustAiApplication.class, args);
+	}
+
+}
